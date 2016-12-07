0d3038fab7d9206fb0af467da661eff38f60ba61
Ivan Uvarov <flashcactus@ya.ru>
2016-12-07 14:47:11 +0300

    counter: add button strober and reversal

--- counter-leds.py
+++ counter-leds.py
@@ -1,7 +1,34 @@
 #!/usr/bin/python3
 from migen import *
+from migen.genlib.fsm import *
 
 import periph
+
+class Strober(Module):
+    def __init__(self):
+        self.inp = Signal()
+        self.strobe = Signal()
+
+        self.submodules.fsm = FSM()
+
+        ###
+        
+        self.fsm.act("IDLE",
+            If(self.inp,
+                NextValue(self.strobe,1),
+                NextState("WAITRELEASE")
+            )
+        )
+
+        self.fsm.act("WAITRELEASE",
+            [
+                NextValue(self.strobe,0),
+                If (~self.inp,
+                    NextState("IDLE")
+                )
+            ]
+        )
+
 
 class _LedTest(Module):
     def __init__(self,platform):
@@ -13,28 +40,44 @@ class _LedTest(Module):
 
         leds = Cat([plat.request("user_led",n) for n in range(10)])
         btn = plat.request("key",0)
+        sw = plat.request("sw",0)
 
-        
+        direction = Signal(reset=1)
+
         ###
+
         self.clock_domains.cd_sys = ClockDomain()
         self.clock_domains.cd_slow = ClockDomain()
         
         self.submodules.convs = [periph.display_convert_7seg() for n in range(6)]
 
-        self.sync.sys += ckdiv_counter.eq(ckdiv_counter+1)
-        self.sync.slow += counter.eq(counter+1)
+        self.submodules.btn_stb = Strober()
+
+        ###
+
+        #btn strobe
+        self.comb += self.btn_stb.inp.eq(~btn)
+        
+        #7seg converter input
+        self.comb += [self.convs[n].value.eq(counter[4*n:4*(n+1)]) for n in range(6)]
+        
+        #output to leds
+        self.comb += leds.eq(counter[:10])
+        self.comb += [digits[n].eq(~self.convs[n].digit) for n in range(6)]
 
         #drive the clocks
         clk50 = plat.request("clk50")
         self.comb += self.cd_sys.clk.eq(clk50)
         self.comb += self.cd_slow.clk.eq(ckdiv_counter[20])
 
-        #7seg converter input
-        self.comb += [self.convs[n].value.eq(counter[4*n:4*(n+1)]) for n in range(6)]
+        #direction
+        self.sync.sys += If(self.btn_stb.strobe, direction.eq(~direction))
 
-        #output to leds
-        self.comb += leds.eq(counter[:10])
-        self.comb += [digits[n].eq(~self.convs[n].digit) for n in range(6)]
+        ## sync
+        self.sync.sys += ckdiv_counter.eq(ckdiv_counter+1)
+        self.sync.slow += If(direction, counter.eq(counter+1)).Else(counter.eq(counter-1))
+
+
 
     
 if __name__ == '__main__':
